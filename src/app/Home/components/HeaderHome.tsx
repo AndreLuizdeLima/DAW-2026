f8daccd7aabaa6ce35af1210d9525e7f0aad6b5a
AndreLuizdeLima <andreluizjp2015@gmail.com>
2026-08-07 21:47:49 -0300

    add title in page

--- src/app/Home/components/HeaderHome.tsx
+++ src/app/Home/components/HeaderHome.tsx
@@ -2,9 +2,15 @@ import { ShoppingCart } from "lucide-react";
 
 const HeaderHome = () => {
   return (
-    <header className="p-5 ">
+    <header className="p-5">
       <div className="flex flex-row justify-between h-24 items-center">
-        <img src="data/logo_lanchonete.svg" className="h-24" />
+        <div className="flex flex-row items-center gap-4">
+          <img src="data/logo_lanchonete.svg" className="h-24" />
+          <div>
+            <h1 className="text-xl font-semibold">Cardápio digital</h1>
+            <p className="text-md text-gray-500">Faça o seu pedido aqui</p>
+          </div>
+        </div>
         <div className="p-3 rounded-full border border-black hover:bg-gray-100 hover:shadow-lg">
           <ShoppingCart />
         </div>
