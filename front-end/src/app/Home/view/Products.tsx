d0b280780ee909ffea1588b29c8f4f2a0f49d77c
AndreLuizdeLima <andreluizjp2015@gmail.com>
2026-09-02 19:53:09 -0300

    ajuste respnsividade

--- front-end/src/app/Home/view/Products.tsx
+++ front-end/src/app/Home/view/Products.tsx
@@ -73,7 +73,7 @@ const Products = () => {
   );
 
   return (
-    <article className="grid grid-cols-4 gap-2">
+    <article className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 my-4">
       {itens.length
         ? itens.map((i, index) => <ProductCard {...i} key={index} />)
         : null}
